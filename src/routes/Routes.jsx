fc719dbb445415963b5ab71b65bccfa56122a972
Sayem92 <sayemmiha123@gmail.com>
2024-02-25 13:40:20 +0600

    add login system

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,24 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../components/Home/Home";
 import Register from "../components/Form/Register";
-
+import Login from "../components/Form/Login";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        // errorElement: <ErrorPage></ErrorPage>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>,
-            },
-            {
-                path: '/register',
-                element: <Register></Register>,
-            }
-            
-            
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    // errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
