685d7627a9733d6caa38449a583775eb330867a0
Sayem92 <sayemmiha123@gmail.com>
2024-02-25 13:52:16 +0600

    add navbar

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
